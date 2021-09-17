6c1bd5af7b9215049d704d6c0ff929a3410c9f64
chougar youva <chougar.abdelhadi@gmail.com>
2021-09-17 13:48:17 +0200

    ajax partie 2 : send request with attribute post

--- JS05_AJAX/AJAX2/ajax.js
+++ JS05_AJAX/AJAX2/ajax.js
@@ -18,6 +18,45 @@ form.addEventListener('submit', function (e) {
         }
     }
     httpRequest.open('POST', './ajax.php', true)
+    // je t'nevoie des infos sous form url
+    // httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+    // var a = link.textContent
+    // encodeURIComponent(a) pour echapper les  caract speciaux
+    // httpRequest.send("city=" + encodeURIComponent(a) + "&nom=Henry")
+
+    // var data = new FormData(form)
+    // data.append('city', 'coucou' )
+    // data.append('name', 'chougar' )
+    // httpRequest.send(data)
+
+    var data = new FormData(form)
+    httpRequest.send(data)
+    
+})
+
+
+
+/*
+var result = document.querySelector('#result')
+var form = document.querySelector('#form')
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()  
+    result.innerHTML = "Chargement..."  
+    var httpRequest = new XMLHttpRequest()
+    
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState === 4) { 
+            result.innerHTML = ''
+            if (httpRequest.status = 200) {
+                result.innerHTML = httpRequest.responseText
+            } else {
+                alert('impossible dae contacter le serveur !')
+            }
+        }
+    }
+    httpRequest.open('POST', './ajax.php', true)
     var data = new FormData(form)
     // var input = document.querySelector('#q') 
     // data.append('q', input.value)
@@ -29,3 +68,4 @@ form.addEventListener('submit', function (e) {
 })
 
 
+*/
